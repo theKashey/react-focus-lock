ba60d53730c6ef8adf9ca2fbfb2ba15a67bd97c6
Anton Korzunov <akorzunov@atlassian.com>
2017-09-01 22:18:30 +1000

    test coverage

--- _tests/focusMerge.spec.js
+++ _tests/focusMerge.spec.js
@@ -1,7 +1,29 @@
 import {expect} from 'chai';
 
-import focusMerge from '../src/utils/focusMerge';
-
+import {newFocus} from '../src/utils/focusMerge';
 
 describe('focus Merge order', () => {
+  it('should move from start to end', () => {
+    // cycle via left
+    expect(newFocus([2, 3, 4], [1, 2, 3, 4, 5], 1, 2)).to.be.equal(2);
+  });
+
+  it('should move from end to start', () => {
+    // cycle via left
+    expect(newFocus([2, 3, 4], [1, 2, 3, 4, 5], 5, 4)).to.be.equal(0);
+  });
+
+  it('should keep direction of move', () => {
+    // cycle via left
+    expect(newFocus([2, 4, 6], [1, 2, 3, 4, 5, 6], 5, 4)).to.be.equal(2);
+  });
+
+  it('should jump back', () => {
+    // jump back
+    expect(newFocus([2,3,4],[1,2,3,4,5],1,4)).to.be.equal(2);
+    // jump back
+    expect(newFocus([2,3,4],[1,2,3,4,5],1,3)).to.be.equal(1);
+  });
+
+
 });
--- src/utils/focusMerge.js
+++ src/utils/focusMerge.js
@@ -2,6 +2,49 @@ import { orderByTabIndex } from './tabOrder';
 import { getCommonParent, getTabbableNodes } from './DOMutils';
 import { getFocusables } from './tabUtils';
 
+export const newFocus = (innerNodes, outerNodes, activeElement, lastNode) => {
+  const cnt = innerNodes.length;
+  const firstFocus = innerNodes[0];
+  const lastFocus = innerNodes[cnt - 1];
+
+  const activeIndex = outerNodes.indexOf(activeElement);
+  const lastIndex = outerNodes.indexOf(lastNode || activeIndex);
+  const lastNodeInside = innerNodes.indexOf(lastNode);
+  const indexDiff = activeIndex - lastIndex;
+  const firstNodeIndex = outerNodes.indexOf(firstFocus);
+  const lastNodeIndex = outerNodes.indexOf(lastFocus);
+
+  // new focus
+  if (activeIndex === -1 || lastNodeInside === -1) {
+    return 0;
+  }
+  // old focus
+  if (!indexDiff && lastNodeInside >= 0) {
+    return lastNodeInside;
+  }
+  // jump out
+  if (indexDiff && Math.abs(indexDiff) > 1) {
+    return lastNodeInside;
+  }
+  // focus above lock
+  if (activeIndex <= firstNodeIndex) {
+    return cnt - 1;
+  }
+  // focus below lock
+  if (activeIndex > lastNodeIndex) {
+    return 0;
+  }
+  // index is inside tab order, but outside Lock
+  if (indexDiff) {
+    if (Math.abs(indexDiff) > 1) {
+      return lastNodeInside;
+    }
+    return (cnt + lastNodeInside + indexDiff) % cnt;
+  }
+  // do nothing
+  return undefined;
+};
+
 const getFocusMerge = (topNode, lastNode) => {
   const activeElement = document.activeElement;
 
@@ -14,45 +57,11 @@ const getFocusMerge = (topNode, lastNode) => {
 
   const outerNodes = orderByTabIndex(getFocusables(commonParent)).map(({ node }) => node);
 
-  const firstFocus = innerNodes[0];
-  const lastFocus = innerNodes[innerNodes.length - 1];
-
-  const activeIndex = outerNodes.indexOf(activeElement);
-  const lastIndex = outerNodes.indexOf(lastNode || activeIndex);
-  const lastNodeInside = innerNodes.map(({ node }) => node).indexOf(lastNode);
-  const indexDiff = activeIndex - lastIndex;
-  const firstNodeIndex = outerNodes.indexOf(firstFocus.node);
-  const lastNodeIndex = outerNodes.indexOf(lastFocus.node);
-
-  // new focus
-  if (activeIndex === -1 || lastNodeInside === -1) {
-    return firstFocus;
+  const newId = newFocus(innerNodes.map(({ node }) => node), outerNodes, activeElement, lastNode);
+  if (newId === undefined) {
+    return newId;
   }
-  // old focus
-  if (!indexDiff && lastNodeInside >= 0) {
-    return innerNodes[lastNodeInside];
-  }
-  // jump out
-  if (indexDiff && Math.abs(indexDiff) > 1) {
-    return innerNodes[lastNodeInside];
-  }
-  // focus above lock
-  if (activeIndex <= firstNodeIndex) {
-    return lastFocus;
-  }
-  // focus below lock
-  if (activeIndex > lastNodeIndex) {
-    return firstFocus;
-  }
-  // index is inside tab order, but outside Lock
-  if (indexDiff) {
-    if (Math.abs(indexDiff) > 1) {
-      return innerNodes[lastNodeInside];
-    }
-    return innerNodes[(innerNodes.length + lastNodeInside + indexDiff) % innerNodes.length];
-  }
-  // do nothing
-  return undefined;
+  return innerNodes[newId];
 };
 
 export default getFocusMerge;
