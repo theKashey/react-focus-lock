ebfd718c72f9e7fa35332d2481da978963652925
Anton Korzunov <akorzunov@atlassian.com>
2017-09-20 10:20:51 +1000

    secure tabIndex and bump focus-lock

--- src/Lock.js
+++ src/Lock.js
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react';
+import React, {PropTypes, Component} from 'react';
 import FocusTrap from './Trap';
 
 class FocusLock extends Component {
@@ -31,7 +31,7 @@ class FocusLock extends Component {
       observed,
     });
 
-  update =() =>
+  update = () =>
     this.setState(prevState => ({
       escapeAttempts: prevState.escapeAttempts + 1,
     }));
@@ -39,20 +39,23 @@ class FocusLock extends Component {
   originalFocusedElement = null;
 
   render() {
-    const { children, disabled } = this.props;
-    const { observed } = this.state;
+    const {children, disabled} = this.props;
+    const {observed} = this.state;
     return (
-      <div
-        ref={this.setObserveNode}
-        onBlur={this.onTrapBlur}
-      >
-        <FocusTrap
-          observed={observed}
-          disabled={disabled}
-          onActivation={this.onActivation}
+      <div>
+        <div tabIndex={ disabled ? -1 : 1} aria-hidden></div>
+        <div
+          ref={this.setObserveNode}
+          onBlur={this.onTrapBlur}
         >
-          {children}
-        </FocusTrap>
+          <FocusTrap
+            observed={observed}
+            disabled={disabled}
+            onActivation={this.onActivation}
+          >
+            {children}
+          </FocusTrap>
+        </div>
       </div>
     );
   }
