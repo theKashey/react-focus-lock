ebe0dcd0db35673d1cd419150bdcb727545a0f0e
Anton Korzunov <akorzunov@atlassian.com>
2017-12-28 21:44:19 +1100

    typescript def

---
+++ react-focus-lock.d.ts
@@ -0,0 +1,35 @@
+declare module 'react-focus-lock' {
+    import * as React from 'react';
+
+    interface Props {
+        disabled?: boolean;
+        /**
+         * will return focus to the previous position on trap disable.
+         */
+        returnFocus?: boolean;
+        /**
+         * enables(or disables) text selection. This also allows not to have ANY focus.
+         */
+        allowTextSelection?: boolean;
+        /**
+         * disables hidden inputs before and after the lock.
+         */
+        noFocusGuards?: boolean;
+
+        children: React.ReactElement<any>
+    }
+
+    interface AutoFocusProps {
+        children: React.ReactElement<any>
+    }
+
+    /**
+     * Traps Focus inside a Lock
+     */
+    export default class ReactFocusLock extends React.Component<Props> {}
+
+    /**
+     * Autofocus on children
+     */
+    export class AutoFocusInside extends React.Component<AutoFocusProps> {}
+}
