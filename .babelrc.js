42a83a2682ae3c2ad90bf6caf443731f49e82776
Remus Mate <rmate@seek.com.au>
2024-01-09 16:42:22 +1100

    use `generatorOpts.shouldPrintComment`

---
+++ .babelrc.js
@@ -0,0 +1,48 @@
+const shouldPrintComment = (val) => /@license|@preserve|[#@]__PURE__/.test(val);
+
+module.exports = {
+  env: {
+    cjs: {
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+      plugins: [
+        "@babel/plugin-proposal-class-properties",
+        "@babel/plugin-transform-runtime",
+        [
+          "transform-react-remove-prop-types",
+          {
+            mode: "wrap",
+          },
+        ],
+      ],
+      generatorOpts: {
+        shouldPrintComment,
+      },
+    },
+    es2015: {
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            modules: false,
+            loose: true,
+          },
+        ],
+        "@babel/preset-react",
+      ],
+      plugins: [
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-class-properties",
+        ["@babel/plugin-transform-runtime", { useESModules: true }],
+        [
+          "transform-react-remove-prop-types",
+          {
+            mode: "wrap",
+          },
+        ],
+      ],
+      generatorOpts: {
+        shouldPrintComment,
+      },
+    },
+  },
+};
