a9be7a4d1a39386141a28bd6e02cc0083c595880
Anton Korzunov <thekashey@gmail.com>
2024-02-15 17:35:52 +1100

    fix: correct d.ts exports

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import FocusLock from './Combination';
 
 export * from './UI';
 
+// no named export yes, as it will interfere with eslint rules
+// export { FocusLock };
+
 export default FocusLock;
